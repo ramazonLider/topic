71bddd15f20f19aa108bdd06b6072632c1f8d1ae
ramazonLider <166911849+ramazonLider@users.noreply.github.com>
2024-09-21 21:38:12 +0500

    dff

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = os.getenv('DEBUG', default=False)
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'), 'localhost', 'topic-theta.vercel.app']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 # Application definition
 
